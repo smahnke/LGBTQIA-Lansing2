6009b4c1da9ba653524fc420742802ce704947fe
smahnke <mahnkes1@msu.edu>
2018-01-23 12:54:47 -0500

    EPSG 4326 conversion

--- metro.js
+++ metro.js
@@ -3,6 +3,6 @@ var siteData = {
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
                                                                                 
 "features": [
-{ "type": "Feature", "properties": { "osm_id": "2632777122", "name": "Trammps Bar", "barrier": null, "highway": null, "ref": null, "address": null, "is_in": null, "place": null, "man_made": null, "other_tags": "\"addr:city\"=>\"Philadelphia\",\"addr:housenumber\"=>\"302\",\"addr:postcode\"=>\"19122\",\"addr:street\"=>\"West Berks Street\",\"amenity\"=>\"social_facility\",\"contact:phone\"=>\"215-339-0900\",\"social_facility\"=>\"food_bank\",\"website\"=>\"http:\/\/www.philabundance.org\"" }, "geometry": { "type": "Point", "coordinates": [ 42.734447, -84.545577 ] } },
+{ "type": "Feature", "properties": { "osm_id": "2632777122", "name": "Trammps Bar", "barrier": null, "highway": null, "ref": null, "address": null, "is_in": null, "place": null, "man_made": null, "other_tags": "\"addr:city\"=>\"Philadelphia\",\"addr:housenumber\"=>\"302\",\"addr:postcode\"=>\"19122\",\"addr:street\"=>\"West Berks Street\",\"amenity\"=>\"social_facility\",\"contact:phone\"=>\"215-339-0900\",\"social_facility\"=>\"food_bank\",\"website\"=>\"http:\/\/www.philabundance.org\"" }, "geometry": { "type": "Point", "coordinates": [ -84.545577, 42.734447 ] } },
 ]
 };
